8ce9ec5cc8256b440eb83f2199a63020ff9cda51
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-12-14 21:57:15 +0530

    header Completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,39 +1,61 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { navitem } from '../../Assets/StaticData/navigation'
-import { MdShoppingBag,MdAccountCircle } from "react-icons/md";
+import { MdShoppingBag, MdAccountCircle, MdShoppingCart, MdSearch } from "react-icons/md";
 import './Header.scss'
 const Header = () => {
   return (
     <header className='Header'>
       <nav className='navigationheader'>
-       
-        <div className='leftsideHeader'>
-          <ul className='headerNav'>
-            {navitem.map((item) => { return <li key={item.id} className='headernavItem'><NavLink to={item.url}>{item.text}</NavLink></li>})}
-          </ul>
-        </div>
-        <div className='logo'>
-          <img src="/logo192.png" alt='' />
-        </div>
-        <div className='rightSideHeader'>
-        {/* <ul className='headerNav'>
-            {navitem.map((item) => { return <li key={item.id} className='headernavItem'><NavLink to={item.url}>{item.text}</NavLink></li>})}
-          </ul> */}
-          {/* <div className='rightSiteheaderElementone'>
-           
-          </div> */}
-          <NavLink  to={'/about'}>About</NavLink>
-            <NavLink to={'/contact'}>Contact</NavLink>
-          <div className='rightSiteheaderElementtwo'>
-            <div className='cart'><MdShoppingBag className=' medium'></MdShoppingBag></div>
-            <div><Link to={'/account'}><MdAccountCircle></MdAccountCircle></Link> </div>
+
+        <div className='headerLeft'>
+          <div class="form-group">
+            <select class="custom-select" name="language" id="language">
+              <option selected value="en"> En</option>
+              <option value="es">Es</option>
+              <option value="hi">Hi</option>
+            </select>
           </div>
+          <div class="form-group search">
+            <input type="text"
+              class="form-control" name="searchItem" id="searchItem" aria-describedby="helpId" placeholder="Search" /><button><MdSearch></MdSearch></button>
+          </div>
+
         </div>
-       
+        <div className='headerCenter'>
+          <div class="form-group">
+            <label>GostStore</label>
+          </div>
+
+        </div>
+        <div className='headerRight'>
+          <Link to={'/signup'} class="btn">Register</Link>
+          <Link to={'/signin'} class="btn">Sign in</Link>
+          <div class="form-icon">
+            <div className='cart-icon'><MdShoppingCart></MdShoppingCart>
+              <span>5</span>
+            </div>
+
+          </div>
+
+
+        </div>
+
       </nav>
+      <div className='headertext'>
+        <p>  loremLaborum nisi do cillum amet id aliqua.</p>
+      </div>
     </header>
   )
 }
 
 export default Header
+
+
+
+
+// const oldnav = ()=>{
+//     {/* <ul className='headerNav'>
+//             {navitem.map((item) => { return <li key={item.id} className='headernavItem'><NavLink to={item.url}>{item.text}</NavLink></li>})}
+//           </ul> */}
+// }
